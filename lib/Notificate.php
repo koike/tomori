cbeecd3ef593bb744bf1bb4f9a8387995b666cd1
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-02-01 15:47:49 +0900

    :up: Updated Notificate.php

--- lib/Notificate.php
+++ lib/Notificate.php
@@ -31,52 +31,61 @@ class Notificate
 
     public static function error($no, $str, $file, $line)
     {
-        $text = urlencode
-        (
-            "[Error (" .
-            $no .
-            ")]\nfile:" .
-            $file .
-            " => line:" .
-            $line .
-            "\n```\n" .
-            $str .
-            "\n```"
-        );
+        $token = getenv('SLACK_TOKEN');
+        if($token != null && $token != '')
+        {
+            $channel = urlencode('#alert');
+            $text = urlencode
+            (
+                "[Error (" .
+                $no .
+                ")]\nfile:" .
+                $file .
+                " => line:" .
+                $line .
+                "\n```\n" .
+                $str .
+                "\n```"
+            );
 
-        $url = 'https://slack.com/api/chat.postMessage?token=' .
-                $token .
-                '&channel=' .
-                $channel .
-                '&text=' .
-                $text;
-        file_get_contents($url);
+            $url = 'https://slack.com/api/chat.postMessage?token=' .
+                    $token .
+                    '&channel=' .
+                    $channel .
+                    '&text=' .
+                    $text;
+            file_get_contents($url);
+        }
     }
 
     public static function exception(\Exception $e)
     {
-        ob_start();
-        var_dump($e);
-        $exception_dump = ob_get_contents();
-        ob_end_clean();
+        $token = getenv('SLACK_TOKEN');
+        if($token != null && $token != '')
+        {
+            ob_start();
+            var_dump($e);
+            $exception_dump = ob_get_contents();
+            ob_end_clean();
 
-        $exception_dump = str_replace('```', '` ` `', $exception_dump);
-        $message = $e->getMessage();
-        $text = urlencode
-        (
-            "[Exception]\n" .
-            $message .
-            "\n```\n" .
-            $exception_dump .
-            "\n```"
-        );
+            $exception_dump = str_replace('```', '` ` `', $exception_dump);
+            $message = $e->getMessage();
+            $text = urlencode
+            (
+                "[Exception]\n" .
+                $message .
+                "\n```\n" .
+                $exception_dump .
+                "\n```"
+            );
 
-        $url = 'https://slack.com/api/chat.postMessage?token=' .
-                $token .
-                '&channel=' .
-                $channel .
-                '&text=' .
-                $text;
-        file_get_contents($url);
+            $url = 'https://slack.com/api/chat.postMessage?token=' .
+                    $token .
+                    '&channel=' .
+                    $channel .
+                    '&text=' .
+                    $text;
+            file_get_contents($url);
+        }
     }
 }
