6b6a2196c91a6633532c109b49a38df290970d82
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-02-02 19:03:51 +0900

    :up: Updated lib/Notificate.php

--- lib/Notificate.php
+++ lib/Notificate.php
@@ -113,8 +113,7 @@ class Notificate
         }
 
         // 無限に再起動するのを防ぐために一旦スリープする
-        sleep(1 * 60);
-        // exec('nohup php tomori.php > /dev/null 2>&1 &', $arr, $res);
-        exec('php tomori.php');
+        sleep(5 * 60);
+        exec('nohup php tomori.php > /dev/null 2>&1 &', $arr, $res);
     }
 }
