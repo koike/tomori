2759ef12002bc6fcfbcb1a9377814ff6836e9907
Kirin MORINO <info@o8o.jp>
2017-02-04 17:00:05 +0900

    :up: Updated lib/phirehose

--- lib/phirehose/Phirehose.php
+++ lib/phirehose/Phirehose.php
@@ -690,7 +690,7 @@ abstract class Phirehose
       $s.= "\r\n";
       
       fwrite($this->conn, $s);
-      // $this->log($s);
+      $this->log($s);
       
       // First line is response
       list($httpVer, $httpCode, $httpMessage) = preg_split('/\s+/', trim(fgets($this->conn, 1024)), 3);
@@ -803,7 +803,7 @@ abstract class Phirehose
    */
   protected function log($message,$level='notice')
   {
-    @error_log('Phirehose: ' . $message, 0);
+    // @error_log('Phirehose: ' . $message, 0);
   }
 
   /**
