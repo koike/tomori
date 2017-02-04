5eccfbe02da0abaee13931846f7b4db9847e34f3
Kirin MORINO <info@o8o.jp>
2017-02-04 16:57:47 +0900

    :up: Updated lib/phirehose

--- lib/phirehose/Phirehose.php
+++ lib/phirehose/Phirehose.php
@@ -677,7 +677,17 @@ abstract class Phirehose
       $authCredentials = $this->getAuthorizationHeader($url,$requestParams);
       
       // Do it
-      $s= "\n\n========== Refresh ==========\n\n";
+      $s = "POST " . $urlParts['path'] . " HTTP/1.1\r\n";
+      $s.= "Host: " . $urlParts['host'] . ':' . $port . "\r\n";		
+      $s .= "Connection: Close\r\n";		
+      $s.= "Content-type: application/x-www-form-urlencoded\r\n";		
+      $s.= "Content-length: " . strlen($postData) . "\r\n";		
+      $s.= "Accept: */*\r\n";		
+      $s.= 'Authorization: ' . $authCredentials . "\r\n";		
+      $s.= 'User-Agent: ' . $this->userAgent . "\r\n";		
+      $s.= "\r\n";		
+      $s.= $postData . "\r\n";		
+      $s.= "\r\n";
       
       fwrite($this->conn, $s);
       $this->log($s);
