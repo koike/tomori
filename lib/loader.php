4201032c335e2f523e856cc138a047e2d37966ec
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-02-09 13:08:55 +0900

    :up: Updated lib/loader.php

--- lib/loader.php
+++ lib/loader.php
@@ -4,18 +4,20 @@ require_once 'vendor/autoload.php';
 require_once 'phirehose/Phirehose.php';
 require_once 'phirehose/OauthPhirehose.php';
 
-require_once 'lib/Analyze.php';
-require_once 'lib/Database.php';
-require_once 'lib/Gist.php';
-require_once 'lib/Notificate.php';
-require_once 'lib/Request.php';
-require_once 'lib/Signature.php';
-require_once 'lib/Streaming.php';
+// require_once /lib/*.php
+foreach(glob(__DIR__ . '/*.php') as $file)
+{
+    if(is_file($file))
+    {
+        require_once $file;
+    }
+}
 
-require_once 'lib/Campaign/PseudoDarkleech.php';
-require_once 'lib/Campaign/EITest.php';
-require_once 'lib/Campaign/EITest2.php';
-require_once 'lib/Campaign/Afraidgate.php';
-require_once 'lib/Campaign/C1.php';
-require_once 'lib/Campaign/C2.php';
-require_once 'lib/Campaign/C3.php';
+// require_once /lib/Campaign/*.php
+foreach(glob(__DIR__ . '/Campaign/*.php') as $file)
+{
+    if(is_file($file))
+    {
+        require_once $file;
+    }
+}
