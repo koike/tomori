015c095d6b9d73239d872e6027d54e05f717f8ed
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-02-14 15:53:03 +0900

    :up: Updated lib/Streaming.php

--- lib/Streaming.php
+++ lib/Streaming.php
@@ -23,6 +23,8 @@ class SampleConsumer extends OauthPhirehose
                 // urlを小文字に変換
                 $url = mb_strtolower($url);
 
+                echo 0;
+
                 // 無限に展開しないように
                 $history = [];
                 // 短縮URLの場合は再帰的に展開する
@@ -91,7 +93,11 @@ class SampleConsumer extends OauthPhirehose
                     $url = $extract;
                 }
 
+                echo 1;
+
                 $tomori = new Analyze($url);
+
+                echo 2;
 
                 // 解析する意味のあるURLか
                 if(!$tomori->exclude_url())
@@ -99,11 +105,17 @@ class SampleConsumer extends OauthPhirehose
                     return;
                 }
 
+                echo 3;
+
                 // 既に解析していないか
                 $url_accessed = DB::table('URL')->where('url', $url)->get();
 
+                echo 4;
+
                 if(empty($url_accessed))
                 {
+                    echo 5;
+
                     $date = date('Y-m-d H:i:s');
 
                     DB::table('URL')
@@ -114,6 +126,8 @@ class SampleConsumer extends OauthPhirehose
                             'created_at'    =>  $date
                         ]
                     );
+
+                    echo 6;
 
                     $response = Request::get($url);
                     $tomori = new Analyze($url);
@@ -130,6 +144,8 @@ class SampleConsumer extends OauthPhirehose
                         echo '[-] ' . $url . PHP_EOL;
                     }
                 }
+
+                echo 7;
             }
         }
     }
