5ba97bcc10a31736bdffb0149940d26cd769c9df
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-02-14 15:44:05 +0900

    :up: Updated lib/Database.php

--- lib/Database.php
+++ lib/Database.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Events\Dispatcher;
 use Illuminate\Container\Container;
+use Illuminate\Database\Events\StatementPrepared;
 use Illuminate\Database\Capsule\Manager as BaseDB;
 
 class DB
 {
     protected static $db = null;
+    
     public static function __callStatic($method, $args)
     {
         if(static::$db == null)
@@ -18,6 +20,16 @@ class DB
                     'driver'    => 'sqlite',
                     'database'  => 'tomori.db'
                 ]
+            );
+
+            $dispatcher = new Dispatcher(new Container);
+            $dispatcher->listen
+            (
+                StatementPrepared::class,
+                function ($event)
+                {
+                    $event->statement->setFetchMode(PDO::FETCH_ASSOC);
+                }
             );
             static::$db->setEventDispatcher(new Dispatcher(new Container));
             static::$db->setAsGlobal();
