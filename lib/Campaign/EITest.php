731c1b71d104c285296f1e7456d292ba9d49a474
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-02-14 14:25:07 +0900

    :up: Updated EITest.php

--- lib/Campaign/EITest.php
+++ lib/Campaign/EITest.php
@@ -21,50 +21,6 @@ class EITest
         // http://小文字.大文字.大文字
         if(preg_match('/https?:\/\/([a-z0-9\-_]+)\.([a-zA-Z0-9\-_]+)\.([a-zA-Z0-9]+)/', $html))
         {
-            $rate += 0.5;
-        }
-
-        // biw, ct, br_fl, tuif, oq
-        if(strpos($html, 'biw=') !== false)
-        {
-            $rate += 0.2;
-        }
-        if(strpos($html, 'ct=') !== false)
-        {
-            $rate += 0.2;
-        }
-        if(strpos($html, 'br_fl=') !== false)
-        {
-            $rate += 0.2;
-        }
-        if(strpos($html, 'tuif=') !== false)
-        {
-            $rate += 0.2;
-        }
-        if(strpos($html, 'oq=') !== false)
-        {
-            $rate += 0.2;
-        }
-
-        if(strpos($html, 'sourceid=') !== false)
-        {
-            $rate += 0.5;
-        }
-        if(strpos($html, 'aqs=') !== false)
-        {
-            $rate += 0.5;
-        }
-
-        if(strpos($html, 'QMvXcJ') !== false)
-        {
-            $rate += 1;
-        }
-        if(strpos($html, 'WrwE0q') !== false)
-        {
-            $rate += 1;
-        }
-        if(strpos($html, 'fPrfJxzFGMSUb-nJDa9') !== false)
-        {
             $rate += 1;
         }
 
