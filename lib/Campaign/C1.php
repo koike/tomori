b506af05db016a0f286490e54759d43d4035c2ff
RintaroKOIKE <koike_rintaro@applibot.co.jp>
2017-02-20 11:51:41 +0900

    :up: Updated lib/Campaign/C1.php

--- lib/Campaign/C1.php
+++ lib/Campaign/C1.php
@@ -5,7 +5,7 @@ class C1
     public static function analyze(string $html) : bool
     {
         // hits?
-        if(preg_match('/<iframe src=[\'|"]https?:\/\/.+hits?/', $html))
+        if(preg_match('/<iframe src=[\'|"]https?:\/\/.+hits\?/', $html))
         {
             return true;
         }
